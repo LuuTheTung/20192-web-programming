281bab2f4791b40b94187acf1ccba0eb4d63f544
LuuTung <orian.insec@gmail.com>
2020-03-10 09:54:41 +0700

    Commit Expression Example

---
+++ week2.php
@@ -0,0 +1,15 @@
+<html>
+    <head> 
+        <title>HTML Check commit</title> </head>
+    <body> 
+        <font size=5 color=”blue”>Welcome To My Page</font>
+        <?php
+        $a = 1;
+        $b = 2;
+        $c = 3;
+        $sum = ($a + $b + $c);
+        print "Sum : $sum";
+        ?>
+        
+    </body>
+</html>
